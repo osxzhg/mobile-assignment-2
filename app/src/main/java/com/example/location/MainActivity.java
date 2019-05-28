1eda6cee09680ac46696093a66b2c3f55cdf4e6d
osxzhg <osxzhg@gmail.com>
2019-05-28 23:33:08 +1200

    add navigation button add google map activity pressure button and then go to google map

--- app/src/main/java/com/example/location/MainActivity.java
+++ app/src/main/java/com/example/location/MainActivity.java
@@ -2,18 +2,50 @@ package com.example.location;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
     private TextView textView;
     private LocationManager locationManager;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    // ...
+
+                    return true;
+                case R.id.navigation_dashboard:
+                    Intent mesg = new Intent();
+                    mesg.setClass(MainActivity.this,MapsActivity.class);
+
+                    if (mesg.resolveActivity(getPackageManager()) != null) {
+                        startActivity(mesg);
+                    }
+
+                    return true;
+                case R.id.navigation_map:
+
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +55,10 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         textView = (TextView) findViewById(R.id.id_textview);
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Log.e("pause","pause");
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
